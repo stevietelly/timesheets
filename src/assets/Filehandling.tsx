4ab111a19ee8c1be0abcf30f3bb42a4c97e8bfad
Stephen Telian <telianstephen@gmail.com>
2024-03-28 15:31:56 +0300

    File Handling, creating and opening

--- src/assets/Filehandling.tsx
+++ src/assets/Filehandling.tsx
@@ -4,9 +4,9 @@ const {ipcRenderer, electron, remote} = window.require("electron");
 const fs = window.require("fs");
 
 
-export async function FileCreator(filename: string, fileContent:{}): Promise<{fullfilled: boolean, err: any | null}>{
+export async function FileCreator(folder: string, filename: string, fileContent:{}): Promise<{fullfilled: boolean, err: any | null}>{
     try {
-        fs.writeFileSync(filename, JSON.stringify(fileContent))
+        fs.writeFileSync(folder + "//" + filename + ".json", JSON.stringify(fileContent))
         return {fullfilled: true, err: null}
     } catch (error) {
         return {fullfilled: false, err: error}
@@ -24,5 +24,30 @@ export const showOpenDialog = async (setStatus: React.Dispatch<React.SetStateAct
     return [canceled, filePaths]
   };
 
+export function CreateNewTimesheet(foldername: string, filename: string): string {
+  FileCreator(foldername, filename, {name: filename})
+  return foldername + filename + ".json";
+}
+const handleOpenFileDialog = async () => {
 
-    
+  const selectedFilePath = await ipcRenderer.invoke('open-file-dialog');
+  
+  if (!selectedFilePath) {
+    return;
+  }
+  return selectedFilePath;
+};
+
+const handleReadFile = (filepath: string) => {
+
+  var obj = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  return obj
+
+};
+
+export async function OpenTimesheet(){
+  const dialog = handleOpenFileDialog();
+  const filepath = await dialog.then((fp: string)=>{return fp})
+  return  filepath;
+
+} 
